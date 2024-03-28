f9dbea43c4f71381653d946a279f87aabeb5029c
Kim Min Sung <1nnovator1105@gmail.com>
2024-03-28 16:46:18 +0900

    feat: init template

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import NotesPicker from '@/components/NotePicker'
+import Notes from '@/components/Notes'
+
+export default function Home() {
+  return (
+    <main className="w-full min-h-screen pb-10">
+      <NotesPicker />
+      <Notes />
+    </main>
+  )
+}
